e08155e711077cda1fe3ad61d00d13ef62a6f4cf
Niklas Lindell <niklas_lindell@hotmail.com>
2018-09-18 14:17:54 +0200

    Counter

---
+++ Assets/Scripts/MeterController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MeterController : MonoBehaviour {
+    
+    public Text counterText;
+    public float speedUp = 2.5f;
+
+
+
+
+    private void Start()
+    {
+        counterText = GetComponent<Text>() as Text;
+    }
+
+
+    void Update () {
+
+        Time.timeScale = speedUp;
+        
+        int speedUpTime = (int)Time.time;
+        counterText.text = speedUpTime + " m";
+		
+	}
+
+
+
+
+}
